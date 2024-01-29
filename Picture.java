94317d4ccb83c6c9710111982fd903227005cd89
SteelRaids <112569099+SteelRaids@users.noreply.github.com>
2024-01-29 08:15:05 -0500

    Removed objects

--- Picture.java
+++ Picture.java
@@ -11,10 +11,10 @@
  */
 public class Picture
 {
-    private Square wall;
-    private Square window;
-    private Triangle roof;
-    private Circle sun;
+    private Square background;
+    //private Square window;
+    //private Triangle roof;
+    //private Circle sun;
     private boolean drawn;
 
     /**
@@ -22,10 +22,10 @@ public class Picture
      */
     public Picture()
     {
-        wall = new Square();
-        window = new Square();
-        roof = new Triangle();  
-        sun = new Circle();
+        background = new Square();
+        //window = new Square();
+        //roof = new Triangle();  
+        //sun = new Circle();
         drawn = false;
     }
 
@@ -35,27 +35,28 @@ public class Picture
     public void draw()
     {
         if(!drawn) {
-            wall.moveHorizontal(-140);
-            wall.moveVertical(20);
-            wall.changeSize(120);
-            wall.makeVisible();
+            background.changeColor("yellow");
+            background.moveHorizontal(-350);
+            background.moveVertical(-150);
+            background.changeSize(999);
+            background.makeVisible();
             
-            window.changeColor("black");
-            window.moveHorizontal(-120);
-            window.moveVertical(40);
-            window.changeSize(40);
-            window.makeVisible();
+            //window.changeColor("black");
+            //window.moveHorizontal(-120);
+            //window.moveVertical(40);
+            //window.changeSize(40);
+            //window.makeVisible();
     
-            roof.changeSize(60, 180);
-            roof.moveHorizontal(20);
-            roof.moveVertical(-60);
-            roof.makeVisible();
+            //roof.changeSize(60, 180);
+            //roof.moveHorizontal(20);
+            //roof.moveVertical(-60);
+            //roof.makeVisible();
     
-            sun.changeColor("yellow");
-            sun.moveHorizontal(100);
-            sun.moveVertical(-40);
-            sun.changeSize(80);
-            sun.makeVisible();
+            //sun.changeColor("yellow");
+            //sun.moveHorizontal(100);
+            //sun.moveVertical(-40);
+            //sun.changeSize(80);
+            //sun.makeVisible();
             drawn = true;
         }
     }
@@ -65,10 +66,10 @@ public class Picture
      */
     public void setBlackAndWhite()
     {
-        wall.changeColor("black");
-        window.changeColor("white");
-        roof.changeColor("black");
-        sun.changeColor("black");
+        background.changeColor("black");
+        //window.changeColor("white");
+        //roof.changeColor("black");
+        //sun.changeColor("black");
     }
 
     /**
@@ -76,9 +77,9 @@ public class Picture
      */
     public void setColor()
     {
-        wall.changeColor("red");
-        window.changeColor("black");
-        roof.changeColor("green");
-        sun.changeColor("yellow");
+        background.changeColor("yellow");
+        //window.changeColor("red");
+        //roof.changeColor("green");
+        //sun.changeColor("yellow");
     }
 }
