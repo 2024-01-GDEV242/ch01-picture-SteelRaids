0ee03610e570d7215e64af1ac2d65fdcefce5749
SteelRaids <112569099+SteelRaids@users.noreply.github.com>
2024-01-29 16:21:23 -0500

    Final Picture

--- Picture.java
+++ Picture.java
@@ -12,9 +12,23 @@
 public class Picture
 {
     private Square background;
-    //private Square window;
-    //private Triangle roof;
-    //private Circle sun;
+    private Circle pacman;
+    private Triangle pacmouth;
+    private Person lives;
+    private Person lives1;
+    private Person lives2;
+    private Circle pellet;
+    private Circle pellet1;
+    private Square leftwall;
+    private Square leftwall1;
+    private Square leftwall2;
+    private Square leftwall3;
+    private Square leftwall4;
+    private Square rightwall;
+    private Square rightwall1;
+    private Square rightwall2;
+    private Square rightwall3;
+    private Square rightwall4;
     private boolean drawn;
 
     /**
@@ -23,9 +37,23 @@ public class Picture
     public Picture()
     {
         background = new Square();
-        //window = new Square();
-        //roof = new Triangle();  
-        //sun = new Circle();
+        pacman = new Circle();
+        pacmouth = new Triangle();
+        lives = new Person();
+        lives1 = new Person();
+        lives2 = new Person();
+        pellet = new Circle();
+        pellet1 = new Circle();
+        leftwall = new Square();
+        leftwall1 = new Square();
+        leftwall2 = new Square();
+        leftwall3 = new Square();
+        leftwall4 = new Square();
+        rightwall = new Square();
+        rightwall1 = new Square();
+        rightwall2 = new Square();
+        rightwall3 = new Square();
+        rightwall4 = new Square();
         drawn = false;
     }
 
@@ -35,28 +63,110 @@ public class Picture
     public void draw()
     {
         if(!drawn) {
-            background.changeColor("yellow");
+            background.changeColor("black");
             background.moveHorizontal(-350);
             background.moveVertical(-150);
             background.changeSize(999);
             background.makeVisible();
             
-            //window.changeColor("black");
-            //window.moveHorizontal(-120);
-            //window.moveVertical(40);
-            //window.changeSize(40);
-            //window.makeVisible();
-    
-            //roof.changeSize(60, 180);
-            //roof.moveHorizontal(20);
-            //roof.moveVertical(-60);
-            //roof.makeVisible();
-    
-            //sun.changeColor("yellow");
-            //sun.moveHorizontal(100);
-            //sun.moveVertical(-40);
-            //sun.changeSize(80);
-            //sun.makeVisible();
+            pacman.changeColor("yellow");
+            pacman.moveHorizontal(-135);
+            pacman.moveVertical(-50);
+            pacman.changeSize(130);
+            pacman.makeVisible();
+            
+            pacmouth.changeColor("black");
+            pacmouth.moveHorizontal(-50);
+            pacmouth.moveVertical(-30);
+            pacmouth.makeVisible();
+            
+            lives.changeColor("yellow");
+            lives.moveHorizontal(120);
+            lives.moveVertical(0);
+            lives.makeVisible();
+            
+            lives1.changeColor("yellow");
+            lives1.moveHorizontal(120);
+            lives1.moveVertical(-100);
+            lives1.makeVisible();
+            
+            lives2.changeColor("yellow");
+            lives2.moveHorizontal(120);
+            lives2.moveVertical(0);
+            lives2.makeVisible();
+            
+            pellet.changeColor("yellow");
+            pellet.moveHorizontal(-80);
+            pellet.moveVertical(100);
+            pellet.changeSize(30);
+            pellet.makeVisible();
+            
+            pellet1.changeColor("yellow");
+            pellet1.moveHorizontal(-80);
+            pellet1.moveVertical(165);
+            pellet1.changeSize(30);
+            pellet1.makeVisible();
+            
+            leftwall.changeColor("blue");
+            leftwall.moveHorizontal(-70);
+            leftwall.moveVertical(0);
+            leftwall.changeSize(60);
+            leftwall.makeVisible();
+            
+            leftwall1.changeColor("blue");
+            leftwall1.moveHorizontal(-70);
+            leftwall1.moveVertical(-60);
+            leftwall1.changeSize(60);
+            leftwall1.makeVisible();
+            
+            leftwall2.changeColor("blue");
+            leftwall2.moveHorizontal(-70);
+            leftwall2.moveVertical(-120);
+            leftwall2.changeSize(60);
+            leftwall2.makeVisible();
+            
+            leftwall3.changeColor("blue");
+            leftwall3.moveHorizontal(-70);
+            leftwall3.moveVertical(-180);
+            leftwall3.changeSize(60);
+            leftwall3.makeVisible();
+            
+            leftwall4.changeColor("blue");
+            leftwall4.moveHorizontal(-70);
+            leftwall4.moveVertical(-240);
+            leftwall4.changeSize(60);
+            leftwall4.makeVisible();
+            
+            rightwall.changeColor("blue");
+            rightwall.moveHorizontal(-290);
+            rightwall.moveVertical(0);
+            rightwall.changeSize(60);
+            rightwall.makeVisible();
+            
+            rightwall1.changeColor("blue");
+            rightwall1.moveHorizontal(-290);
+            rightwall1.moveVertical(-60);
+            rightwall1.changeSize(60);
+            rightwall1.makeVisible();
+            
+            rightwall2.changeColor("blue");
+            rightwall2.moveHorizontal(-290);
+            rightwall2.moveVertical(-120);
+            rightwall2.changeSize(60);
+            rightwall2.makeVisible();
+            
+            rightwall3.changeColor("blue");
+            rightwall3.moveHorizontal(-290);
+            rightwall3.moveVertical(-180);
+            rightwall3.changeSize(60);
+            rightwall3.makeVisible();
+            
+            rightwall4.changeColor("blue");
+            rightwall4.moveHorizontal(-290);
+            rightwall4.moveVertical(-240);
+            rightwall4.changeSize(60);
+            rightwall4.makeVisible();
+
             drawn = true;
         }
     }
@@ -67,9 +177,23 @@ public class Picture
     public void setBlackAndWhite()
     {
         background.changeColor("black");
-        //window.changeColor("white");
-        //roof.changeColor("black");
-        //sun.changeColor("black");
+        pacman.changeColor("white");
+        pacmouth.changeColor("black");
+        lives.changeColor("white");
+        lives1.changeColor("white");
+        lives2.changeColor("white");
+        pellet.changeColor("white");
+        pellet1.changeColor("white");
+        leftwall.changeColor("white");
+        leftwall1.changeColor("white");
+        leftwall2.changeColor("white");
+        leftwall3.changeColor("white");
+        leftwall4.changeColor("white");
+        rightwall.changeColor("white");
+        rightwall1.changeColor("white");
+        rightwall2.changeColor("white");
+        rightwall3.changeColor("white");
+        rightwall4.changeColor("white");
     }
 
     /**
@@ -77,9 +201,23 @@ public class Picture
      */
     public void setColor()
     {
-        background.changeColor("yellow");
-        //window.changeColor("red");
-        //roof.changeColor("green");
-        //sun.changeColor("yellow");
+        background.changeColor("black");
+        pacman.changeColor("yellow");
+        pacmouth.changeColor("black");
+        lives.changeColor("yellow");
+        lives1.changeColor("yellow");
+        lives2.changeColor("yellow");
+        pellet.changeColor("yellow");
+        pellet1.changeColor("yellow");
+        leftwall.changeColor("blue");
+        leftwall1.changeColor("blue");
+        leftwall2.changeColor("blue");
+        leftwall3.changeColor("blue");
+        leftwall4.changeColor("blue");
+        rightwall.changeColor("blue");
+        rightwall1.changeColor("blue");
+        rightwall2.changeColor("blue");
+        rightwall3.changeColor("blue");
+        rightwall4.changeColor("blue");
     }
 }
